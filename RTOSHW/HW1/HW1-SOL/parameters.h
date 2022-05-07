0f784026e5c25edd8f1a4cf5c014061022923243
Matteo <kirito.poppox@gmail.com>
2022-05-07 20:49:53 +0200

    started working on solution

---
+++ RTOSHW/HW1/HW1-SOL/parameters.h
@@ -0,0 +1,9 @@
+#define TICK_TIME 100000
+#define SENSOR_QUEUE_NAME   "/sensor_queue"
+#define ACTUATOR_QUEUE_NAME "/actuator_queue"
+#define REFERENCE_QUEUE_NAME "/reference_queue"
+#define WDOG_QUEUE_NAME "/watchdog_queue"
+#define QUEUE_PERMISSIONS 0660
+#define MAX_MESSAGES 1000
+#define MAX_MSG_SIZE 16
+#define BUF_SIZE 5
