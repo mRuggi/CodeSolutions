3d83fd44b5b5f07fa7dff7ef6d1758da9d26492e
Matteo Ruggiero <kirito.poppox@gmail.com>
2022-06-08 21:25:18 +0200

    Add files via upload
    Second version of porting, TODO solved, need to implement the slack time part.

--- RTOSHW/HW2/main_homework.c
+++ RTOSHW/HW2/main_homework.c
@@ -16,7 +16,9 @@
 #define TASK_NUM 5
 
 /* Periodo di Base */
-#define BASE_PERIOD_MS 10
+#define ENCODER_PERIOD 5
+#define REAL_TIME_PERIOD ENCODER_PERIOD/2
+#define SCOPE_PERIOD ENCODER_PERIOD*100
 
 /*Priorità dei task*/
 #define PRIORITY_RT (tskIDLE_PRIORITY + 3)
@@ -62,17 +64,10 @@ static struct _rising_edge rising_edge;
 static struct _round_time round_time;
 static struct _slack_rt1 slack_rt1;
 static struct _slack_rt2 slack_rt2;
-static float periods[TASK_NUM];
-
 
 void main_homework(void)
 {
 	
-	/* Inizializzare i periodi dei task */
-	periods[0]=periods[1]=BASE_PERIOD_MS; //periodo di scope e diagnostic
-	periods[2]=(BASE_PERIOD_MS/100); //periodo di encoder
-	periods[3]=periods[4]=(BASE_PERIOD_MS/200); //periodi dei task rt1 e rt2
-
 	//creazione mutex
 	enc_data.lock=xSemaphoreCreateMutex();
 	rising_edge.lock=xSemaphoreCreateMutex();
@@ -108,12 +103,12 @@ static void encoder(void* pvParameters)
 	unsigned int prev_slit = 0;
 
 	/* Randomized period (75-750 RPM) */
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
 	unsigned int semi_per = (rand() % 10) + 1;
-	printf("[ENCODER] genero semiper %d", semi_per);
+	printf("[ENCODER] genero semiper %d\n", semi_per);
 	
 	TickType_t xNextWakeTime;
-	const TickType_t xBlockTime = pdMS_TO_TICKS(periods[2]);
+	const TickType_t xBlockTime = pdMS_TO_TICKS(ENCODER_PERIOD);
 	xNextWakeTime = xTaskGetTickCount();
 
 	for( ;; ) {
@@ -144,10 +139,10 @@ static void rt_1(void* pvParameters){
 	
 	(void) pvParameters;
 
-	printf("rt1 Start\n");
+	printf("RT1 Start\n");
 
 	TickType_t xNextWakeTime;
-	const TickType_t xBlockTime = pdMS_TO_TICKS(periods[3]);
+	const TickType_t xBlockTime = pdMS_TO_TICKS(REAL_TIME_PERIOD);
 	xNextWakeTime = xTaskGetTickCount();
 
 	xSemaphoreTake(rising_edge.lock,portMAX_DELAY);
@@ -158,12 +153,15 @@ static void rt_1(void* pvParameters){
 
 	for(;;){
 		vTaskDelayUntil(&xNextWakeTime, xBlockTime );
+
 		xSemaphoreTake(enc_data.lock,portMAX_DELAY);
 			if( last_value == 0 && enc_data.slit == 1){
 				last_value = 1;
+
 				xSemaphoreTake(rising_edge.lock,portMAX_DELAY);
 				rising_edge.count++;
 				xSemaphoreGive(rising_edge.lock);
+
 			}
 			else if(last_value == 1 && enc_data.slit == 0){
 				last_value = 0;
@@ -177,10 +175,10 @@ static void rt_2(void* pvParameters){
 
 	(void) pvParameters;
 
-	printf("rt2 Start\n");
+	printf("RT2 Start\n");
 
 	TickType_t xNextWakeTime;
-	const TickType_t xBlockTime = pdMS_TO_TICKS(periods[4]);
+	const TickType_t xBlockTime = pdMS_TO_TICKS(REAL_TIME_PERIOD);
 	xNextWakeTime = xTaskGetTickCount();
 
 	TickType_t time_home;
@@ -203,7 +201,7 @@ static void rt_2(void* pvParameters){
 				else{	
 					time_home=xTaskGetTickCount();
 					xSemaphoreTake(round_time.lock,portMAX_DELAY);
-					round_time.time_diff = time_home - last_time_home;
+					round_time.time_diff = pdTICKS_TO_MS(time_home-last_time_home);
 					xSemaphoreGive(round_time.lock);
 					last_time_home = time_home;	
 				}
@@ -220,14 +218,14 @@ static void scope(void* pvParameters){
 
 	(void) pvParameters;
 
-	printf("scope Start\n");
+	printf("Scope Start\n");
 
 	TickType_t xNextWakeTime;
-	const TickType_t xBlockTime = pdMS_TO_TICKS(periods[0]);
+	const TickType_t xBlockTime = pdMS_TO_TICKS(SCOPE_PERIOD);
 	xNextWakeTime = xTaskGetTickCount();
 
 	unsigned int count=0;
-	float diff_us = 0;
+	float diff_ms = 0;
 	unsigned int rpm = 0;
 
 	for(;;){
@@ -240,10 +238,10 @@ static void scope(void* pvParameters){
 	printf("Rising Edge Counter : %d\t",count);
 
 	xSemaphoreTake(round_time.lock,portMAX_DELAY);
-	diff_us = (float)(round_time.time_diff / 1000);			//difference in microseconds
+	diff_ms = round_time.time_diff;			//difference in milliseconds
 	xSemaphoreGive(round_time.lock);
 
-	rpm = (unsigned int)(60*1000000/diff_us);
+	rpm = (unsigned int)(60*1000/diff_ms);
 	printf("RPM : %u\n",rpm);	
 
 	}
