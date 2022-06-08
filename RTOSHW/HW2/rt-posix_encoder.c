483940389c8fafcd008c31323b6d17a2188ed83e
Matteo Ruggiero <kirito.poppox@gmail.com>
2022-06-08 08:35:55 +0200

    Add files via upload

---
+++ RTOSHW/HW2/rt-posix_encoder.c
@@ -0,0 +1,308 @@
+#include <sys/time.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define TICK_PERIOD 5000
+#define NSEC_PER_SEC 1000000000ULL
+
+/* periodic thread */
+struct periodic_thread {
+	int index;
+	struct timespec r;
+	int period;
+	int wcet;
+	int priority;
+};
+
+/* encoder */
+struct enc_str
+{
+	unsigned int slit;		//valori oscillanti tra 0 e 1
+	unsigned int home_slit;	//1 se in home, 0 altrimenti
+	pthread_mutex_t lock;
+};
+static struct enc_str enc_data;
+
+struct _rising_edge{
+	unsigned int count;
+	pthread_mutex_t lock;
+};
+static struct _rising_edge rising_edge;
+
+struct _round_time{
+	unsigned long int time_diff;
+	pthread_mutex_t lock;
+};
+static struct _round_time round_time;
+
+
+/***************************** Functions ************************************/
+
+static inline void timespec_add_us(struct timespec *t, uint64_t d)
+{
+    d *= 1000;
+    t->tv_nsec += d;
+    t->tv_sec += t->tv_nsec / NSEC_PER_SEC;
+    t->tv_nsec %= NSEC_PER_SEC;
+}
+
+void wait_next_activation(struct periodic_thread * thd)
+{
+    clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &(thd->r), NULL);
+    timespec_add_us(&(thd->r), thd->period);
+}
+
+void start_periodic_timer(struct periodic_thread * thd, uint64_t offs)
+{
+    clock_gettime(CLOCK_REALTIME, &(thd->r));
+    timespec_add_us(&(thd->r), offs);
+}
+
+unsigned long int difference_ns(struct timespec *ts1, struct timespec *ts2){
+	long int diff_sec, diff_nsec;
+	diff_sec =(ts1->tv_sec - ts2->tv_sec);
+	diff_nsec = (ts1->tv_nsec - ts2->tv_nsec);
+	return diff_sec*NSEC_PER_SEC + diff_nsec;
+}
+
+/***************************** THREADS ************************************/
+
+void* rt_task1(void* parameter){
+	struct periodic_thread *th = (struct periodic_thread *) parameter;
+	start_periodic_timer(th,TICK_PERIOD/2);
+	
+	pthread_mutex_lock(&rising_edge.lock);
+	rising_edge.count = 0;
+	pthread_mutex_unlock(&rising_edge.lock);
+	
+	int last_value=0;
+	
+	while (1) {
+		wait_next_activation(th);
+		
+		pthread_mutex_lock(&enc_data.lock);
+		if( last_value == 0 && enc_data.slit == 1){
+			last_value = 1;
+			
+			pthread_mutex_lock(&rising_edge.lock);
+			rising_edge.count++;
+			pthread_mutex_unlock(&rising_edge.lock);
+			
+		}
+		else if(last_value == 1 && enc_data.slit == 0){
+			last_value = 0;
+		}
+		pthread_mutex_unlock(&enc_data.lock);
+	}
+}
+
+void* rt_task2(void* parameter){
+	struct periodic_thread *th = (struct periodic_thread *) parameter;
+	start_periodic_timer(th,TICK_PERIOD/2);
+	
+	struct timespec time_home;
+	struct timespec last_time_home;
+	
+	int first_measure = 1;
+	int last_home_slit = 0;
+		
+	while (1) {
+		wait_next_activation(th);
+		
+		pthread_mutex_lock(&enc_data.lock);
+		
+		if(enc_data.home_slit == 1 && last_home_slit == 0){
+			last_home_slit = 1;
+			if(first_measure){
+				clock_gettime(CLOCK_REALTIME, &last_time_home);
+				first_measure = 0;
+			}
+			else{	
+				clock_gettime(CLOCK_REALTIME, &time_home);
+					
+				pthread_mutex_lock(&round_time.lock);
+				round_time.time_diff = difference_ns(&time_home,&last_time_home);
+				pthread_mutex_unlock(&round_time.lock);
+			
+				last_time_home = time_home;	
+			}
+		}
+		else if(enc_data.home_slit == 0){
+			last_home_slit = 0;
+		}
+		
+		pthread_mutex_unlock(&enc_data.lock);
+		
+	}	
+}
+
+void* scope(void* parameter){
+	struct periodic_thread *th = (struct periodic_thread *) parameter;
+	start_periodic_timer(th,TICK_PERIOD*100);	
+	
+	unsigned int count=0;
+	float diff_us = 0;
+	unsigned int rpm = 0;
+	
+	while (1) {
+		wait_next_activation(th);
+		
+		pthread_mutex_lock(&rising_edge.lock);
+		count = rising_edge.count;
+		pthread_mutex_unlock(&rising_edge.lock);
+		
+		printf("Rising Edge Counter : %d\t",count);
+		
+		pthread_mutex_lock(&round_time.lock);
+		diff_us = round_time.time_diff / 1000;			//difference in microseconds
+		pthread_mutex_unlock(&round_time.lock);
+		
+		rpm = (unsigned int)(60*1000000/diff_us);
+		
+		//printf("diff : %f\t",diff_us);				//DEBUG
+		printf("RPM : %u\n",rpm);	
+	}
+	
+}
+
+void* enc(void* parameter){
+	printf("Encoder Start\n");
+	
+	pthread_mutex_lock(&enc_data.lock);
+	
+	enc_data.slit = 0;
+	enc_data.home_slit = 0;
+	
+	pthread_mutex_unlock(&enc_data.lock);
+	
+	
+	unsigned int count = 0;
+	unsigned int slit_count = 0;		
+	unsigned int prev_slit = 0;
+	
+	/* Randomized period (75-750 RPM) */
+	srand(time(NULL));
+	unsigned int semi_per = (rand() % 10) + 1;	
+	//semi_per = 5;								//DEBUG
+	
+	struct periodic_thread *th = (struct periodic_thread *) parameter;
+	start_periodic_timer(th,TICK_PERIOD);
+	
+	while (1) {
+		wait_next_activation(th);
+		
+		pthread_mutex_lock(&enc_data.lock);
+		
+		prev_slit = enc_data.slit;
+		if (count%semi_per == 0) {
+			enc_data.slit++;
+			enc_data.slit%=2;
+		}
+
+		if (prev_slit==0&&enc_data.slit==1) 					//fronte di salita
+			slit_count=(++slit_count)%8;
+
+		if (slit_count==0) enc_data.home_slit=enc_data.slit;
+		else enc_data.home_slit=0;
+
+		//printf("%d:\t\t %d %d\n",count,enc_data.slit,enc_data.home_slit);	//DEBUG encoder
+		count++;
+		
+		pthread_mutex_unlock(&enc_data.lock);
+
+	}
+	
+}
+
+int main(){
+	struct periodic_thread th_e;
+	struct periodic_thread th_rt1;
+	struct periodic_thread th_rt2;
+	struct periodic_thread th_buddy;
+	
+	pthread_t thread_enc;
+	pthread_t thread_rt1;
+	pthread_t thread_rt2;
+	pthread_t thread_buddy;
+	
+	pthread_attr_t myattr;
+	struct sched_param myparam;
+	
+	pthread_mutexattr_t mutex_attr;
+	
+	/* Mutex */
+	pthread_mutexattr_init (&mutex_attr);
+	pthread_mutexattr_setprotocol (&mutex_attr, PTHREAD_PRIO_INHERIT);	// can be PTHREAD_PRIO_NONE, PTHREAD_PRIO_INHERIT, PTHREAD_PRIO_PROTECT
+	//pthread_mutexattr_setprioceiling(&mutex_attr, pceiling);		// if PTHREAD_PRIO_PROTECT mutex (set pceiling)
+	pthread_mutex_init(&enc_data.lock,&mutex_attr);
+	pthread_mutex_init(&rising_edge.lock,&mutex_attr);
+	pthread_mutex_init(&round_time.lock,&mutex_attr);
+
+	
+	/* Encoder periodic thread */
+	th_e.index = 1;
+	th_e.period = TICK_PERIOD;
+	th_e.priority = 11;
+	pthread_attr_init(&myattr);
+	pthread_attr_setschedpolicy(&myattr, SCHED_FIFO);
+	myparam.sched_priority = th_e.priority;
+	pthread_attr_setschedparam(&myattr, &myparam);  
+	pthread_attr_setinheritsched(&myattr, PTHREAD_EXPLICIT_SCHED);
+	
+	pthread_create(&thread_enc, &myattr, enc, &th_e);
+	
+	
+	/* RT1 periodic thread */
+	th_rt1.index = 2;
+	th_rt1.period = TICK_PERIOD/2;
+	th_rt1.priority = 12;
+	myparam.sched_priority = th_rt1.priority;
+	pthread_attr_setschedparam(&myattr, &myparam);  	
+	 
+	pthread_create(&thread_rt1, &myattr, rt_task1, &th_rt1);
+	
+	
+	/* RT2 periodic thread */
+	th_rt2.index = 3;
+	th_rt2.period = TICK_PERIOD/2;
+	th_rt2.priority = 12;
+	myparam.sched_priority = th_rt2.priority;
+	pthread_attr_setschedparam(&myattr, &myparam);  	
+	 
+	pthread_create(&thread_rt2, &myattr, rt_task2, &th_rt2);
+	
+	
+	/* buddy thread */
+	th_buddy.index = 4;
+	th_buddy.period = TICK_PERIOD*2;
+		
+	pthread_create(&thread_buddy,NULL,scope,&th_buddy);
+	
+	
+	/* Clear attr*/
+	pthread_mutexattr_destroy(&mutex_attr);
+	pthread_attr_destroy(&myattr);
+	
+	/* Wait user exit commands*/
+	while (1) {
+   		if (getchar() == 'q') break;
+  	}
+	
+  	/* Clear */
+  	pthread_mutex_destroy(&enc_data.lock);
+  	pthread_mutex_destroy(&rising_edge.lock);
+  	pthread_mutex_destroy(&round_time.lock);
+  	
+  	pthread_kill(thread_enc,0);
+  	pthread_kill(thread_rt1,0);
+  	pthread_kill(thread_rt2,0);
+  	
+  	printf("EXIT!\n");
+
+	return 0;
+}
